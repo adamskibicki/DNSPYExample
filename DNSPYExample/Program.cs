0bcd938a559ba47bed26c31ba238dc3e13006339
adamskibicki <adam-skibicki@outlook.com>
2019-04-02 19:39:25 +0200

    Example finished

--- DNSPYExample/Program.cs
+++ DNSPYExample/Program.cs
@@ -1,15 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace DNSPYExample
 {
     class Program
     {
+        private const string PAGE_URL = "https://programowanienaostro.pl";
+
+        private const string MESSAGE = "Press any key to download page content";
+
         static void Main(string[] args)
         {
+            MainAsync(args).GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            Console.WriteLine(MESSAGE);
+
+            Console.ReadKey();
+
+            HttpClient httpClient = new HttpClient();
+
+            var response = await httpClient.GetAsync(PAGE_URL);
+
+            string content = await response.Content.ReadAsStringAsync();
+
+            Console.WriteLine(content);
+
+            Console.ReadKey();
         }
     }
 }
